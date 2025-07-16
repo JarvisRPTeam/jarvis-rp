7a6538338cd0ff7eb202208e0c4efac02cb061fa
RostBerry <shadowrostik@gmail.com>
2025-07-16 20:03:00 +0300

    #JAR-50 Create admin panel, add user tab

---
+++ dotnet/resources/Main/AdminPanel/PlayerInfoModel.cs
@@ -0,0 +1,10 @@
+namespace Main.AdminPanel
+{
+    public class PlayerInfoModel
+    {
+        public long Id { get; set; }
+        public string Nickname { get; set; }
+        public string Role { get; set; }
+        public bool IsOnline { get; set; }
+    }
+}
